73c223bed0c9253405978be6c1aa0257f6561324
Natalia <natalia.ab.ca@gmail.com>
2023-12-26 22:47:29 +0300

    task1 - DONE

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,20 @@
 ﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void numbers(int m, int n)
+{
+    if (m > n)
+        return;
+    if (m % 1 == 0)
+    {
+        Console.Write($"{m}, ");
+    }
+    numbers(m + 1, n);
+}
+
+
+
+numbers(m, n);
--- task3/Program.cs
+++ task3/Program.cs
@@ -1,1 +1,24 @@
 ﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+Console.Write("Задайте произвольный массив из 4 чисел: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[4];
+int i = 0;
+
+void Array(int number)
+{
+if (array[i] < array.Length) 
+    return;
+     if (i == 0)
+    {    array[i] = number % 10;
+         number /= 10;
+    
+    }
+    Console.Write(array[i]);
+   
+
+}
+
+
+Console.Write(array[i]);
