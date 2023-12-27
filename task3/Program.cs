6b813b16c7181183a6fd59749dc5c2e1408139ee
Natalia <natalia.ab.ca@gmail.com>
2023-12-27 21:47:17 +0300

    task3 - массив моздала, как его перевернуть не через цикл, а с использованием рекурсии - не смогла справиться

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,8 +1,79 @@
 ﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+ int[] GenerateArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
 
-Console.Write("Задайте произвольный массив из 4 чисел: ");
+void PrintArray(int[] arrayForPrint)
+{
+    System.Console.Write("[ ");
+    for (int i = 0; i < arrayForPrint.Length; i++)
+    {
+        System.Console.Write(arrayForPrint[i] + " ");
+    }
+    System.Console.Write(" ]");
+    System.Console.WriteLine();
+} 
+
+
+
+/* 
+void PrintTurnedArra(int[] arrayForPrint2)
+{
+    System.Console.Write("[ ");
+    for (int i = 0; i < arrayForPrint2.Length; i++)
+    {
+        System.Console.Write(arrayForPrint[i] + " ");
+    }
+    System.Console.Write(" ]");
+    System.Console.WriteLine(); 
+
+ void PrintTurnedArray(int array[i], int size)
+{
+    int currentDigit = array[i] % 10;
+    if (array[i] > 0) return;
+    {
+        array[i] /= 10;
+        Console.Write(currentDigit + ",");
+    }
+
+} 
+}
+ */
+int[] myArray = GenerateArray(5, 0, 9);
+PrintArray(myArray);
+/* System.Console.WriteLine(Reverse); */
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Console.Write("Задайте произвольный массив из 4 чисел: ");
 int number = Convert.ToInt32(Console.ReadLine());
-// поменять через GENERATE ARRAY
+                                                            // поменять через GENERATE ARRAY
 int[] array = new int[4];
 int i = 0;
 
@@ -19,6 +90,4 @@ if (array[i] < array.Length)
    
 
 }
-
-
-Console.Write(array[i]);
+*/
